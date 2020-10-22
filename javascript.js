966ab7182e3a03e4efc88207d204fa245332fb0a
MLange2603 <lange260304@gmail.com>
2020-10-22 17:35:58 +0200

    init

--- javascript.js
+++ javascript.js
@@ -66,80 +66,19 @@ function closediv() {
     }   
 };
 
-function gallery_big_1800() {
-    var gallery = document.getElementById("gallery_big_1800");
+function showgallery_and_scroll_to_element(number, hash) {
+    var gallery = document.getElementById("gallery_big_"+ number);
     if(gallery) {
         if(gallery.style.visibility == "hidden") {
             gallery.style.visibility = "visible";
             } else {
                 gallery.style.visibility = "hidden";
             }
-        }       
-    };
-
-function gallery_big_2205() {
-    var gallery = document.getElementById("gallery_big_2205");
-    if(gallery) {
-        if(gallery.style.visibility == "hidden") {
-            gallery.style.visibility = "visible";
-            } else {
-                gallery.style.visibility = "hidden";
-            }
-        }       
-    };
-
-function gallery_big_2070() {
-    var gallery = document.getElementById("gallery_big_2070");
-    if(gallery) {
-        if(gallery.style.visibility == "hidden") {
-            gallery.style.visibility = "visible";
-            } else {
-                gallery.style.visibility = "hidden";
-            }
-        }       
-    };
-
-function gallery_big_1404() {
-    var gallery = document.getElementById("gallery_big_1404");
-    if(gallery) {
-        if(gallery.style.visibility == "hidden") {
-            gallery.style.visibility = "visible";
-            } else {
-                gallery.style.visibility = "hidden";
-            }
-        }       
-    };
-
-function gallery_big_1701() {
-    var gallery = document.getElementById("gallery_big_1701");
-    if(gallery) {
-        if(gallery.style.visibility == "hidden") {
-            gallery.style.visibility = "visible";
-            } else {
-                gallery.style.visibility = "hidden";
-            }
-        }       
-    };
-
-function Home() {
-
-    const xhr = new XMLHttpRequest();
-        const container = document.getElementsByClassName('maincontainer');
-    
-        xhr.onload = function() {
-            if (this.status === 200) {
-                maincontainer.innerHTML = xhr.responseText;
-            } else {
-                console.warn('Seite konnte nicht geladen werden');
-            }
-        };
-    
-        xhr.open('get', 'home.html');
-        xhr.send();
+        }
+    location.hash = "#" + hash;
 };
 
-function weitereinfos() {
-
+function load_content(contenturl) {
     const xhr = new XMLHttpRequest();
         const container = document.getElementsByClassName('maincontainer');
     
@@ -151,84 +90,46 @@ function weitereinfos() {
             }
         };
     
-        xhr.open('get', 'weitereinfos.html');
-        xhr.send();
-
-    var menu = document.getElementById("menu");
-    if(menu) {
-        if(menu.style.visibility == "visible") {
-        menu.style.visibility = "hidden";
-            } 
-    }   
-};
-
-function buildings() {
-
-    const xhr = new XMLHttpRequest();
-        const container = document.getElementsByClassName('maincontainer');
-    
-        xhr.onload = function() {
-            if (this.status === 200) {
-                maincontainer.innerHTML = xhr.responseText;
-            } else {
-                console.warn('Seite konnte nicht geladen werden');
-            }
-        };
-    
-        xhr.open('get', 'buildings.html');
-        xhr.send();
-
-    var menu = document.getElementById("menu");
-    if(menu) {
-        if(menu.style.visibility == "visible") {
-        menu.style.visibility = "hidden";
-            } 
-    }   
-};
-
-function gallery() {
-
-    const xhr = new XMLHttpRequest();
-        const container = document.getElementsByClassName('maincontainer');
-    
-        xhr.onload = function() {
-            if (this.status === 200) {
-                maincontainer.innerHTML = xhr.responseText;
-            } else {
-                console.warn('Seite konnte nicht geladen werden');
-            }
-        };
-    
-        xhr.open('get', 'gallery.html');
-        xhr.send();
-
-    var menu = document.getElementById("menu");
-    if(menu) {
-        if(menu.style.visibility == "visible") {
-        menu.style.visibility = "hidden";
-            } 
-    }   
-};
-
-function boats() {
-
-    const xhr = new XMLHttpRequest();
-        const container = document.getElementsByClassName('maincontainer');
-    
-        xhr.onload = function() {
-            if (this.status === 200) {
-                maincontainer.innerHTML = xhr.responseText;
-            } else {
-                console.warn('Seite konnte nicht geladen werden');
-            }
-        };
-    
-        xhr.open('get', 'boats.html');
+        xhr.open('get', contenturl + ".html");
         xhr.send();
     var menu = document.getElementById("menu");
     if(menu) {
         if(menu.style.visibility == "visible") {
         menu.style.visibility = "hidden";
             } 
-    }   
+    }
 };
+
+function load_content_and_scroll_to_hash(contenturl, hash) {
+    const xhr = new XMLHttpRequest();
+        const container = document.getElementsByClassName('maincontainer');
+    
+        xhr.onload = function() {
+            if (this.status === 200) {
+                maincontainer.innerHTML = xhr.responseText;
+            } else {
+                console.warn('Seite konnte nicht geladen werden');
+            }
+        };
+    
+        xhr.open('get', contenturl + ".html");
+        xhr.send();
+    var menu = document.getElementById("menu");
+    if(menu) {
+        if(menu.style.visibility == "visible") {
+        menu.style.visibility = "hidden";
+            } 
+    }
+    location.hash = '#' + hash 
+};
+
+$(".scrollLeft").on("click", function(e) {
+    $(document).scrollLeft($(this).parent().next().offset().top);
+    // $(this).parent().next() // this is the next div container.
+    return false; // prevent anchor
+});
+
+
+function scrolling(hash) {
+    location.hash = hash
+}
